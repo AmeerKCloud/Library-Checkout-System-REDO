854b055eeedda4fc235b9821e92798dc20b1d099
Ameer Khan <ameer.khan.linkedin@gmail.com>
2026-01-24 21:46:20 -0500

    Update test_main.py

---
+++ tests/test_main.py
@@ -0,0 +1,25 @@
+# Project Description
+# Simulate a simple library where users can borrow and return books.
+
+# Requirements
+# Your final program must:
+# ✓ Include a Book class
+# - Title, author, available (boolean)
+# ✓ Include a Library class
+# - Stores books in a dictionary: {book_id: Book_object}
+# - Methods: show_books(), borrow_book(), return_book()
+# ✓ Include a User class
+# - Tracks borrowed books
+# - Method: borrow(), return_book()
+# ✓ Use user input for:
+# - Selecting books
+# - Borrowing
+# - Returning
+# ✓ Use modules
+# - models.py — classes
+# - library_data.py — starting book list
+# - main.py — main loop
+
+
+
+
