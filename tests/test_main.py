39553f438b62172b7ddf45c255546f02d228f4c0
AmeerKCloud <ameer.khan.linkedin@gmail.com>
2026-01-28 10:53:03 -0500

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -24,17 +24,17 @@
 
 #---------------------------------------------⬇️JSON-Related (+ explanations)------------------------------------------------
 
-# from src.modules.config import DATA_DIR #⬅️ giving error message
+from src.modules.config import DATA_DIR #⬅️ giving error message
 
 #---------------------------------------------JSON-Related (+ explanations)⬆️------------------------------------------------
 
 
 # ----------------------⬇️ Main Program -------------------------
 
-import sys
-print(sys.path)
+# import sys
+# print(sys.path)
 
-# print(DATA_DIR)
+print(DATA_DIR)
 
 
 #TODO: 
