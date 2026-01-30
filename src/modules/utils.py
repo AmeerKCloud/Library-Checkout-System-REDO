ae9d5a1e61f801992e673c9263e9804a1cadfdba
Ameer Khan <ameer.khan.linkedin@gmail.com>
2026-01-30 11:09:52 -0500

    Update utils.py

--- src/modules/utils.py
+++ src/modules/utils.py
@@ -30,7 +30,7 @@ import json
 # - From there, we can step “up” one level to reach the project root (library checkout system/), then into data/.
 # - Add this in test_main.py (under the imports):
 
-#⬇️ Get the absolute path to the project root (one level up from tests/)
+#⬇️ Get the absolute path to the project root (two levels up from modules/src/)
 PRJECT_ROOT = Path(__file__).resolve().parents[2] 
 
 #⬇️ Build the full path to data/library_books.json
