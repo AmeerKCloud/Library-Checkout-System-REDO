86ad1c0cc57186375edc30cf3a3b76c735961876
AmeerKCloud <ameer.khan.linkedin@gmail.com>
2026-01-31 08:31:51 -0500

    Update models.py

--- src/modules/models.py
+++ src/modules/models.py
@@ -2,6 +2,11 @@ from utils import load_library_books
 
 # print(load_library_books())
 
-class Books:
+class Book:
+    """
+    Displays all available book titles.
+    Checks if a book is available or not.
+    Returns True is it is, False if its not.
+    """
     def __init__(self):
         pass
