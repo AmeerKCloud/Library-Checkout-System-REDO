1d9dd067ac535732471de3771d22c255a8c71e47
AmeerKCloud <ameer.khan.linkedin@gmail.com>
2026-01-31 08:41:00 -0500

    Update models.py

--- src/modules/models.py
+++ src/modules/models.py
@@ -8,7 +8,23 @@ class Book:
     Checks if a book is available or not.
     Returns True if it is, False if its not.
     """
-    def __init__(self, book_title, book_author):
+    def __init__(self, book_title, book_author, library_data):
         self.book_title = book_title
         self.book_author = book_author
+        self.library_data = library_data
+        pass
+
+
+class Library:
+    """
+    Docstring for Library
+    """
+    def __init__(self):
+        pass
+
+class User:
+    """
+    Docstring for User
+    """
+    def __init__(self):
         pass
