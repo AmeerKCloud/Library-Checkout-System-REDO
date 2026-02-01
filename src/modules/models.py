16b5cc78cf66bf9d44ece2a6de7d566ab1b59e78
AmeerKCloud <ameer.khan.linkedin@gmail.com>
2026-02-01 09:19:39 -0500

    Update models.py

--- src/modules/models.py
+++ src/modules/models.py
@@ -52,7 +52,7 @@ class Library:
 
         for item in self.library_data:
             if item["title"] == self.book_title and item["author"] == self.book_author:
-                if item["availability"] == True:
+                if item["available"] == True:
                     print(f"{self.book_title} is available.")
 
     def return_book(self, book_title, book_author):
