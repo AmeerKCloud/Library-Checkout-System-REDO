370641f90e0cd9a8833f9c91774a8bc6982c0220
AmeerKCloud <ameer.khan.linkedin@gmail.com>
2026-02-02 09:59:21 -0500

    Update models.py

--- src/modules/models.py
+++ src/modules/models.py
@@ -79,8 +79,8 @@ class User:
     """
 
     user_books_history = {
-        "borrowed": [],
-        "returned": [],
+        "borrowed": {},
+        "returned": {},
     }
 
     def __init__(self, user_name, title, author, date):
@@ -92,7 +92,7 @@ class User:
 
     def borrowed_book(self):
         """Makes a list of all of a users borrowed books."""
-        borrowed_books[] = {}
+        
 
 
 
