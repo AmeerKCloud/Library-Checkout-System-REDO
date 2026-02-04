d901ea4fd788bec96aa752e8d7481ea0ec29647c
Ameer Khan <ameer.khan.linkedin@gmail.com>
2026-02-04 08:21:56 -0500

    Update models.py

--- src/modules/models.py
+++ src/modules/models.py
@@ -108,9 +108,9 @@ class User:
 
     def view_borrowed_history(self, user_name):
         self.user_name = user_name
-        
+
         if self.user_name not in User.user_books_history["borrowed"]:
-            print(f"Sorry, {self.user_name} has no currently borrowed books to show. ☹️")
+            print(f"\nSorry, {self.user_name} has no currently borrowed books to show. ☹️")
         else:
             print(f"All currently borrowed books for {self.user_name}:")
             for item in User.user_books_history["borrowed"]:
