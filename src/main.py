a218f58481cfe7b54d2f8157bd992b9b2783f8f5
Ameer Khan <ameer.khan.linkedin@gmail.com>
2026-02-04 08:15:04 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -63,6 +63,8 @@ while True:
 
         elif menu_1_options == 'c':
             menu_2_options = input("Choose one of the following:\n'b' to view currently borrowed titles\n'r' to view all returned titles:\n").lower()
+            if menu_2_options == 'b':
+                user.view_borrowed_history()
 
         else:
             break
