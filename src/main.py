2bffd9ef908ad1ace1a328326bdeee47dd560b99
Ameer Khan <ameer.khan.linkedin@gmail.com>
2026-02-19 09:30:04 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -124,11 +124,15 @@ while True:
 
 # 2) Date input: Currently here
 # - Trying to find a way to include borrowed & returned dates to user book history (RESOLVED ✅)
-# - Need to now include 'date' module and input formatting to 'utils.py'
+# - Need to now include 'date' module and input formatting to 'utils.py' (RESOLVED ✅)
 
 # 3) When 'borrowed' books history becomes empty, it should show: 'Sorry, {self.user_name} has no currently borrowed books to show. ☹️'
 # - Currently, it only prints this in the beginning before the user even borrows any book
 # - But when he does borrow a book and then returns it & 'borrowed books history' becomes empty, it no longer prints this, but prints an empty format
+
+# 4) When a user returns a book, everything works, but this still prints: 'NOT WORKING!'
+# - Also, returned raw list prints 2x, too
+# - Find out whats causing this and fix it.
 
 # 4) Add finishing touches to the program:
 # - Clean up the code and condense it where possible.
